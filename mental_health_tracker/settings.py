3ec0725e33ff72e300fecdd3af806c0848b0ceab
stefarion <stefanustanjaya230105@gmail.com>
2024-08-28 16:24:40 +0700

    Allowed Host update

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k3%bkwqjb(f96*2$a1!nk021pe(cp1*98w_9n7ut*ryx+i@506
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "stefanus-tan-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
